ce27d27e9ac6918634caaa34629f1275cbc96d84
geniustesda <390131208@qq.com>
2017-05-16 01:35:10 +0800

    fix

--- app_crawl/app_ranking.py
+++ app_crawl/app_ranking.py
@@ -3,29 +3,31 @@ import requests
 import re
 import math
 
-j = 1
-#查看每一页都多少个APP，用于决定我们要翻多少页
+
 html = requests.get('http://app.mi.com/topList')#获取网页的源代码
-label = re.findall('<p class="app-desc">', html.text, re.S)#打开网页源码发现每个App名称前面有<p class="app-desc">，
-# 所有统计这个标示的个数就可以知道每一页有多少个APP啦
-pageAppNum = len(label)#label的长度就代表了每一页APP的个数
+label = re.findall('<p class="app-desc">', html.text, re.S)#打开网页源码发现每个App名称后面都有<p class="app-desc">，
+# 所以统计这个标签的个数就可以知道每一页有多少个APP啦
+pageAppNum = len(label)#通过len()方法来计算label的个数，从而获取当前页面App的个数
+# print pageAppNum #计算一页的最大app数量
 
-# print pageAppNum
+print u'请输入期望获取的App数目：'
+appNum = int(raw_input()) #手动输入app数量
+pageNum = math.ceil(float(appNum)/pageAppNum) #将输入的期望值除以每一页最大值获得总页数,ceil是为了获取整数页数
 
-print u'请输入App的数目：'
-appNum = int(raw_input())
-#根据要爬取的APP个数决定要翻多少页
-pageNum = math.ceil(float(appNum) / pageAppNum)
 
+print "打印到第%d"%pageNum+"页："
+
+j = 1 #初始化一个变量
 for i in range(1, int(pageNum + 1)):
-    html = requests.get('http://app.mi.com/topList?page=' + str(i))#括号内是每一页的连接地址
-    # print html.text
-    title = re.findall('</a><h5><a href=".*?">(.*?)</a></h5><p class="app-desc">', html.text, re.S)#找对应的APP的名称在网页中的位置，并用正则表达式来获取，如果看不懂，那你该好好去看看正则表达式了
-    #将每一页的APP按先后顺序输出，知道第100个
+    html = requests.get('http://app.mi.com/topList?page=' + str(i))#通过递增i的值来获取下一页的app
+    # print html #打印页面
+    title = re.findall('</a><h5><a href=".*?">(.*?)</a></h5><p class="app-desc">', html.text, re.S)#找对应的APP的名称在网页中的位置，并用正则表达式来获取
+    #将每一页的APP按先后顺序输出
+
+    #打印输出的格式
     for each in title:
         if j <= appNum:
             print str(j) + ":" + each
-        j = j + 1
-        
-        
-        
+        j += 1
+
+
