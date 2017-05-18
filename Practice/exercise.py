6df168120c138388037a4401c2ef06e702f6cb29
geniustesda <390131208@QQ.COM>
2017-05-18 15:45:05 +0800

    practice

---
+++ Practice/exercise.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from bs4 import BeautifulSoup
+from bs4 import SoupStrainer
+import urllib2
+
+
+content = urllib2.urlopen("http://weixin.sogou.com/").read().decode('utf-8')
+# print(content)
+#目标区域
+only_body = SoupStrainer("body")
+soup = BeautifulSoup(content,"html.parser",parse_only=only_body)
+for item in soup.find_all('a'):
+    print(item.get('title'))
+    # print(item.get('href'))
