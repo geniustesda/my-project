c7e84abda6242d57d8cc425d5682f0b4a78aaac0
geniustesda <390131208@qq.com>
2017-06-18 00:09:34 +0800

    fix字符串和图像大小问题

--- ToolKit/img2txt.py
+++ ToolKit/img2txt.py
@@ -1,3 +1,4 @@
+# -*- coding:utf-8 -*-
 from PIL import Image
 import argparse
 
@@ -7,7 +8,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('file')     #输入文件
 parser.add_argument('-o', '--output')   #输出文件
 parser.add_argument('--width', type = int, default = 80) #输出字符画宽
-parser.add_argument('--height', type = int, default = 80) #输出字符画高
+parser.add_argument('--height', type = int, default = 55) #输出字符画高
 
 #获取参数
 args = parser.parse_args()
