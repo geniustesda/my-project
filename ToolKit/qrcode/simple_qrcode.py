ba72b81651d1e2735df749f0cc0d9f24c62444df
geniustesda <390131208@QQ.COM>
2017-06-28 13:46:01 +0800

    mhttp://www.github.comhttp://www.github.comhttp://www.github.com

---
+++ ToolKit/qrcode/simple_qrcode.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+import qrcode
+
+# 方法一
+# 实例化对象并保存
+img = qrcode.make("http://www.github.com")
+img.save("test1.png")
+
+
+# 方法二
+# 实例化对象并编写详细参数
+qr = qrcode.QRCode(
+    version =1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+    )
+
+# 添加二维码信息
+qr.add_data("http://www.github.com")
+qr.make(fit=True)
+
+# 编译并保存二维码图片
+img = qr.make_image()
+img.save("./test2.png")
