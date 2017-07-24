83bcae56444e75d0c58a18535e8b45179f77a9d3
geniustesda <390131208@qq.com>
2017-07-25 00:20:47 +0800

    fix and add some files

--- scrapy_project/zufang/zufang/settings.py
+++ scrapy_project/zufang/zufang/settings.py
@@ -66,7 +66,7 @@ ROBOTSTXT_OBEY = True
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
 #    'zufang.pipelines.ZufangPipeline': 300,
-    'zufang.pipelines.ZufangPipeline': 300
+    'zufang.pipelines.ZufangPipeline': 300,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
